91e755d3a7b9b46d1a0f6ee78872bfd7960e6e02
Jonathan Rothberg <jon@join.build>
2025-04-02 16:32:35 -0700

    Update

--- config/plugins/telescope/default.nix
+++ config/plugins/telescope/default.nix
@@ -14,7 +14,6 @@
           enable = true;
           override_generic_sorter = true;
           override_file_sorter = true;
-          case_mode = "smart_case";
         };
         ui-select = {
           enable = true;
