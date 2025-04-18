a6d3c631572214d18e70cd641c0bf29f6e20ac6c
Jonathan Rothberg <jon@join.build>
2025-04-17 22:36:48 -0700

    Comment out harpoon

--- config/plugins/editor/harpoon.nix
+++ config/plugins/editor/harpoon.nix
@@ -1,62 +1,62 @@
 {
-  plugins.harpoon = {
-    enable = true;
-    enableTelescope = true;
-    keymapsSilent = true;
-    keymaps = {
-      addFile = "<leader>ha";
-      toggleQuickMenu = "<C-e>";
-      navFile = {
-        "1" = "<leader>hj";
-        "2" = "<leader>hk";
-        "3" = "<leader>hl";
-        "4" = "<leader>hm";
-      };
-    };
-  };
+  # plugins.harpoon = {
+  #   enable = true;
+  #   enableTelescope = true;
+  #   keymapsSilent = true;
+  #   keymaps = {
+  #     addFile = "<leader>ha";
+  #     toggleQuickMenu = "<C-e>";
+  #     navFile = {
+  #       "1" = "<leader>hj";
+  #       "2" = "<leader>hk";
+  #       "3" = "<leader>hl";
+  #       "4" = "<leader>hm";
+  #     };
+  #   };
+  # };
 
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>mm";
-      action = ":lua require('harpoon.ui').toggle_quick_menu()<CR>";
-      #    lua = true;
-      options = {
-        silent = true;
-        desc = "Mark Menu";
-      };
-    }
+  # keymaps = [
+  #   {
+  #     mode = "n";
+  #     key = "<leader>mm";
+  #     action = ":lua require('harpoon.ui').toggle_quick_menu()<CR>";
+  #     #    lua = true;
+  #     options = {
+  #       silent = true;
+  #       desc = "Mark Menu";
+  #     };
+  #   }
 
-    {
-      mode = "n";
-      key = "<leader>ma";
-      action = ":lua require('harpoon.mark').add_file()<CR>";
-      #    lua = true;
-      options = {
-        silent = true;
-        desc = "Mark File";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>mn";
-      action = ":lua require('harpoon.ui').nav_next()<CR>";
-      #    lua = true;
-      options = {
-        silent = true;
-        desc = "Next Mark";
-      };
-    }
+  #   {
+  #     mode = "n";
+  #     key = "<leader>ma";
+  #     action = ":lua require('harpoon.mark').add_file()<CR>";
+  #     #    lua = true;
+  #     options = {
+  #       silent = true;
+  #       desc = "Mark File";
+  #     };
+  #   }
+  #   {
+  #     mode = "n";
+  #     key = "<leader>mn";
+  #     action = ":lua require('harpoon.ui').nav_next()<CR>";
+  #     #    lua = true;
+  #     options = {
+  #       silent = true;
+  #       desc = "Next Mark";
+  #     };
+  #   }
 
-    {
-      mode = "n";
-      key = "<leader>mp";
-      action = ":lua require('harpoon.ui').nav_prev()<CR>";
-      #    lua = true;
-      options = {
-        silent = true;
-        desc = "Prev Mark";
-      };
-    }
-  ];
+  #   {
+  #     mode = "n";
+  #     key = "<leader>mp";
+  #     action = ":lua require('harpoon.ui').nav_prev()<CR>";
+  #     #    lua = true;
+  #     options = {
+  #       silent = true;
+  #       desc = "Prev Mark";
+  #     };
+  #   }
+  # ];
 }
